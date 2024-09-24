9d4a3b94152ed18d479590faba5d12d909b2264b
iamhectorsosa <hello@hectorsosa.me>
2024-09-24 11:12:48 +0200

    Updating print report

--- print_report.go
+++ print_report.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"sort"
+	"strconv"
 )
 
 type Page struct {
@@ -23,6 +26,11 @@ func printReport(pages map[string]int, baseURL string) {
 		count := page.Count
 		fmt.Printf("Found %d internal links to %s\n", count, url)
 	}
+
+	err := generateCSVReport(sortedPages, "report.csv")
+	if err != nil {
+		fmt.Println("Error generating CSV report:", err)
+	}
 }
 
 func sortPages(pages map[string]int) []Page {
@@ -37,4 +45,34 @@ func sortPages(pages map[string]int) []Page {
 		return pagesSlice[i].Count > pagesSlice[j].Count
 	})
 	return pagesSlice
+}
+
+func generateCSVReport(pages []Page, filename string) error {
+	// Create a new CSV file
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Create a new CSV writer
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write header to the CSV file
+	header := []string{"URL", "Count"}
+	if err := writer.Write(header); err != nil {
+		return err
+	}
+
+	// Write the page data to the CSV file
+	for _, page := range pages {
+		row := []string{page.URL, strconv.Itoa(page.Count)}
+		if err := writer.Write(row); err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("CSV report generated successfully: %s\n", filename)
+	return nil
 }
